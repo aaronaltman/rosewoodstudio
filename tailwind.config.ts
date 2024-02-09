2162e0b4dae589c7bb73dc536b3fe1312f936ca2
aka <aaronkaltman@gmail.com>
2024-02-09 11:44:58 -0500

    go go gadget webstie

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,7 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   daisyui: {
-    themes: ["light", "dark", "cupcake"],
+    themes: ["light", "dark", "cupcake", "dim"],
   },
   theme: {
     extend: {
