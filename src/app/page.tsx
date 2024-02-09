814e074186a34dfbf186650bed5a1ff1944e0c19
aka <aaronkaltman@gmail.com>
2024-02-09 13:02:35 -0500

    new nav maybe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Carousel from "@/components/global-ui/carousel";
 import Footer from "@/components/global-ui/footer";
 import Header from "@/components/global-ui/header";
+import SecondHeader from "@/components/global-ui/second-header";
 import HomeHero from "@/components/home-ui/home-hero";
 import HomeServices from "@/components/home-ui/home-services";
 import Image from "next/image";
@@ -8,7 +9,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <Header />
+      <SecondHeader />
       <main className="">
         <HomeHero />
         <Carousel />
